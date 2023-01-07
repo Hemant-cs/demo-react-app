2f854fc9020d49be4d8365c8d9de721489a8a62e
Hemant-cs <hemantchaurasia555@gmail.com>
2023-01-07 20:00:11 +0530

    removing bug

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,30 +1,39 @@
 import React from "react";
+import './Table.css'
 
 const Table = (props) => {
-    return (
-        <>
-            <table className="table">
-                <thead>
-                    <tr>
-                        <th scope="col">feels_like</th>
-                        <th scope="col">humidity</th>
-                        <th scope="col">max_temp</th>
-                        <th scope="col">min_temp</th>
-                        <th scope="col">sunrise</th>
-                        <th scope="col">sunset</th>
-                        <th scope="col">temp</th>
-                        <th scope="col">wind_degrees</th>
-                        <th scope="col">wind_speed</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        {props.weatherDetails ? console.log(props.weatherDetails.feels_like):null}
-                    </tr>
-                </tbody>
-            </table>
-        </>
-    )
-}
+  return (
+    <>
+      <table className="table mt-30">
+        <thead>
+          <tr>
+            <th scope="col">Feels Like</th>
+            <th scope="col">Humidity</th>
+            <th scope="col">Max Temp</th>
+            <th scope="col">Min Temp</th>
+            <th scope="col">Sunrise</th>
+            <th scope="col">Sunset</th>
+            <th scope="col">Temp</th>
+            <th scope="col">Wind Degrees</th>
+            <th scope="col">Wind Speed</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+          <td>{props.weatherDetails.feels_like}</td>
+          <td>{props.weatherDetails.humidity}</td>
+          <td>{props.weatherDetails.max_temp}</td>
+          <td>{props.weatherDetails.min_temp}</td>
+          <td>{props.weatherDetails.sunrise}</td>
+          <td>{props.weatherDetails.sunset}</td>
+          <td>{props.weatherDetails.temp}</td>
+          <td>{props.weatherDetails.wind_degrees}</td>
+          <td>{props.weatherDetails.wind_speed}</td>
+          </tr>
+        </tbody>
+      </table>
+    </>
+  );
+};
 
 export default Table;
